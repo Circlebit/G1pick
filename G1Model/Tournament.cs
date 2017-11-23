a352ebb1a63ca6e0755cffbce8292abb07734797
Paul Seibel <paul.seibel@gmail.com>
2017-11-23 11:58:08 +0100

    Matches in Block as 2d array

--- G1Model/Tournament.cs
+++ G1Model/Tournament.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.ObjectModel;
+using System.Diagnostics;
 
 namespace G1Model
 {
@@ -10,11 +11,24 @@ namespace G1Model
         public Block BlockB { get; set; }
 
 
-        public Tournament(int blocksize = 10)
+        public Tournament(int blocksize = 5)
         {
             BlockSize = blocksize;
             BlockA = new Block(blocksize);
             BlockB = new Block(blocksize);
+
+            BlockA.Wrestlers[0] = new Wrestler("AAA");
+            BlockA.Wrestlers[1] = new Wrestler("BBB");
+            BlockA.Wrestlers[2] = new Wrestler("CCC");
+            BlockA.Wrestlers[3] = new Wrestler("DDD");
+            BlockA.Wrestlers[4] = new Wrestler("EEE");
+
+            BlockB.Wrestlers[0] = new Wrestler("111");
+            BlockB.Wrestlers[1] = new Wrestler("222");
+            BlockB.Wrestlers[2] = new Wrestler("333");
+            BlockB.Wrestlers[3] = new Wrestler("444");
+            BlockB.Wrestlers[4] = new Wrestler("555");
+
 
         }
 
@@ -23,17 +37,24 @@ namespace G1Model
 
     public class Block
     {
-        public Collection<Wrestler> Wrestlers { get; set; }
-        public Match[] Matches { get; set; }
+        public Wrestler[] Wrestlers { get; set; }
+        public Match[,] Matches { get; set; }
         
 
         public Block(int blocksize)
         {
-            Wrestlers = new Collection<Wrestler>();
-            for (int i = 0; i < blocksize; i++)
+            Wrestlers = new Wrestler[blocksize];
+            Matches = new Match[blocksize,blocksize];
+
+            foreach (var wrestler in Wrestlers)
             {
-                Wrestlers.Add(new Wrestler(null));
+                Debug.WriteLine(wrestler.Name);
             }
+        }
+
+        public void GenerateMatches()
+        {
+
         }
 
     }
