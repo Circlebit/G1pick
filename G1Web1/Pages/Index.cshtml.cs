e8140307865bee8ef9733321b2c30230b7169c01
Paul Seibel <paul.seibel@gmail.com>
2017-11-23 11:57:00 +0100

    Added RazorPage Web Project.

---
+++ G1Web1/Pages/Index.cshtml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using G1Model;
+
+namespace G1Web1.Pages
+{
+    public class IndexModel : PageModel
+    {
+        public Tournament G1 { get; set; }
+
+        public void OnGet()
+        {
+            G1 = new Tournament(5);
+        }
+    }
+}
