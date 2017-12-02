65ab6b7b3a2eca993919d8d3409356c7f33bc4df
Paul Seibel <paul.seibel@gmail.com>
2017-12-02 19:36:58 +0100

    test table is correctly built in the razorpage

--- G1Web1/Pages/Index.cshtml.cs
+++ G1Web1/Pages/Index.cshtml.cs
@@ -14,7 +14,25 @@ namespace G1Web1.Pages
 
         public void OnGet()
         {
-            G1 = new Tournament();
+            G1 = new Tournament(10);
+
+            G1.BlockA.Wrestlers = new Wrestler[] {
+                new Wrestler("AAA"),
+                new Wrestler("BBB"),
+                new Wrestler("CCC"),
+                new Wrestler("DDD"),
+                new Wrestler("EEE"),
+                new Wrestler("FFF"),
+                new Wrestler("GGG"),
+                new Wrestler("HHH"),
+                new Wrestler("III"),
+                new Wrestler("JJJ")
+            };
+            G1.BlockA.GenerateMatches();
+
+
+            //G1.BlockB.Wrestlers = new Wrestler[] { new Wrestler("QQQ"), new Wrestler("WWW"), new Wrestler("XXX"), new Wrestler("YYY"), new Wrestler("ZZZ") };
+            //G1.BlockB.GenerateMatches();
         }
     }
 }
